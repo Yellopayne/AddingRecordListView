820fab4e41a845a22c95e921bedb2dbffcaf4c3c
Mati <qant97@gmail.com>
2017-06-08 18:28:48 +0200

    New Switch Button

--- app/src/main/java/yello/putinto2/MessageReceiver.java
+++ app/src/main/java/yello/putinto2/MessageReceiver.java
@@ -1,6 +1,8 @@
 package yello.putinto2;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,10 +10,13 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MessageReceiver extends AppCompatActivity {
 
@@ -20,6 +25,8 @@ public class MessageReceiver extends AppCompatActivity {
     private static ArrayList<String> arrayL = new ArrayList<String>();
     private static ListView lv;
     private static Button btn2,goBack;
+    private static Boolean deleteRecord=false;
+    private static Switch sw1;
 
 
     @Override
@@ -30,13 +37,14 @@ public class MessageReceiver extends AppCompatActivity {
         displayList();
         removeRecord();
         goBack();
-        displayMessage();
+        deleteOnClick();
 
 
 
     }
 
     public  void displayList(){
+
         String fristActivityValue=getIntent().getExtras().getString("value");
         arrayL.add(fristActivityValue);
         ArrayAdapter<String> ap = new ArrayAdapter<String>(this,
@@ -46,28 +54,33 @@ public class MessageReceiver extends AppCompatActivity {
         lv.setAdapter(ap);
     }
     public void removeRecord(){
-        btn2=(Button)findViewById(R.id.button2);
+        sw1 =(Switch)findViewById(R.id.switcher);
+        sw1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-
-            btn2.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (!arrayL.isEmpty()) {
-                        arrayL.remove(0);
-                        ArrayAdapter<String> ap = new ArrayAdapter<String>(getApplicationContext(),
-                                android.R.layout.simple_list_item_1,
-                                arrayL);
-                        lv = (ListView) findViewById(R.id.listaDanych);
-                        lv.setAdapter(ap);
-
-                    } else {
-                        Toast.makeText(getApplicationContext(), "DataBase is Empty :C", Toast.LENGTH_SHORT).show();
-                    }
+                if(sw1.isChecked()){
+                    deleteRecord=true;
+                    Toast.makeText(getApplicationContext(),
+                            "Tap a record to delete it",
+                            Toast.LENGTH_SHORT)
+                            .show();
+                }
+                else {
+                    deleteRecord=false;
+                    Toast.makeText(getApplicationContext(),
+                            "Now you cannot delete Records :C",
+                            Toast.LENGTH_SHORT)
+                            .show();
 
 
                 }
-            });
+            }
+        });
         }
+
+
+
 
     public void goBack(){
         goBack=(Button)findViewById(R.id.goBackButton);
@@ -82,16 +95,22 @@ public class MessageReceiver extends AppCompatActivity {
     }
 
 
-    public void displayMessage(){
+    public void deleteOnClick(){
         lv =(ListView)findViewById(R.id.listaDanych);
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String messageFromArray = arrayL.get(position);
-                Toast.makeText(getApplicationContext(),messageFromArray,Toast.LENGTH_SHORT).show();
+                if(deleteRecord) {
+                    ArrayAdapter<String> ap = new ArrayAdapter<String>(getApplicationContext(),
+                            android.R.layout.simple_list_item_1,
+                            arrayL);
+                    lv = (ListView) findViewById(R.id.listaDanych);
+                    arrayL.remove(position);
+                    lv.setAdapter(ap);
+                }
+;
             }
         });
     }
-
 
 }
